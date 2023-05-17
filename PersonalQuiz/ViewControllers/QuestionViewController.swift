9af2418074372ca6c989fd6bedcbe805dc2c5672
Alexey Turulin <repakuku@icloud.com>
2023-05-17 14:51:28 -0400

    Homework

--- PersonalQuiz/ViewControllers/QuestionViewController.swift
+++ PersonalQuiz/ViewControllers/QuestionViewController.swift
@@ -42,6 +42,11 @@ final class QuestionViewController: UIViewController {
         rangedSlider.maximumValue = answerCount
         rangedSlider.value = answerCount / 2
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let resultVC = segue.destination as? ResultViewController else { return }
+        resultVC.answerChosen = answerChosen
+    }
 
     // MARK: - IBActions
     @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -8,14 +8,23 @@
 import UIKit
 
 final class ResultViewController: UIViewController {
-
+    
+    // MARK: - IBOutlets
+    @IBOutlet var animalLabel: UILabel!
+    @IBOutlet var definitionLabel: UILabel!
+    
+    // MARK: - Properties
+    var answerChosen: [Answer]!
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.hidesBackButton = true
+        
+        showResultAnimal()
     }
     
-    
-
+    // MARK: - IBActions
     @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
         dismiss(animated: true)
     }
@@ -24,3 +33,26 @@ final class ResultViewController: UIViewController {
         print("ResultViewController is deallocated")
     }
 }
+
+//MARK: - Private Methods
+private extension ResultViewController {
+    func showResultAnimal() {
+        var animalCount: [Animal: Int] = [:]
+        
+        for answer in answerChosen {
+            animalCount[answer.animal] = (animalCount[answer.animal] ?? 0) + 1
+        }
+        
+        let sortedAnimalCount = animalCount.sorted { $0.value > $1.value }
+        guard let resultAnimal = sortedAnimalCount.first?.key else { return }
+        
+        updateUI(with: resultAnimal)
+    }
+    
+    func updateUI(with animal: Animal) {
+        animalLabel.text = "Вы - \(animal.rawValue)"
+        definitionLabel.text = animal.definition
+    }
+}
+ 
+
